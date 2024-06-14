2e662d9d9b4331ae028c50ee86e0d957a8c4564d
bernard.tatin@outlook.fr <BernardTatin>
2024-06-14 10:28:48 +0200

    Faut que ça swingue, et en couleur

---
+++ ca-bouge/vector-of-objects.rkt
@@ -0,0 +1,77 @@
+#lang sketching
+
+; https://github.com/processing/processing-docs/tree/master/content/examples/Basics/Arrays/ArrayObjects
+; Vector of objects.
+
+(define (display-all . lst)
+    (for-each (lambda(e)
+        (display e))
+            lst))
+
+(define bl-colors (vector-immutable "White" "Yellow" "Red" "Green" "Blue"))
+(define bl-colors-len (vector*-length bl-colors))
+(define get-rand-color
+    (lambda ()
+        (let ((idx [random bl-colors-len]))
+          (display-all "rand idx -> " idx " to int " (inexact->exact (floor idx)) "\n")
+          (vector-ref bl-colors (inexact->exact (floor idx))))))
+
+; Demonstrates how to create a vector of objects.
+
+(class Blob Object
+  ; "Constructor"
+  (init-field [x-offset 0]
+              [y-offset 0]
+              [x        0]
+              [y        0]
+              [unit     1]
+              [x-dir    1]
+              [y-dir    1]
+              [speed    1]
+              [bl-color "White"])
+  (super-new)    ; super class initialization
+
+  ; Methods
+  (define/public (update)
+    (:= x (+ x (* speed x-dir)))
+    (unless (<= 0 x unit)
+      (*= x-dir -1)
+      (:= x (+ x x-dir))
+      (:= y (+ y y-dir)))
+
+    (:= y (+ y (* speed y-dir)))
+    (unless (<= 0 y unit)
+      (*= y-dir -1)
+      (:= y (+ y y-dir))))
+
+  (define/public (draw)
+    (fill bl-color)
+    (ellipse (+ x x-offset) (+ y y-offset) 6 6)))
+
+;;; ---------------
+
+(define unit  40)
+(define count 0)
+(define blobs (vector))
+
+(define (setup)
+  (size 640 360)
+  (frame-rate 60)
+  (no-stroke)
+  (:= blobs  (for*/vector  ; for* is a nested loop
+                 ([x (/ width  unit)]
+                  [y (/ height unit)])
+               (new Blob
+                    [bl-color (get-rand-color)]
+                    [x-offset (* x   unit)]
+                    [y-offset (* y   unit)]
+                    [x        (* 1/2 unit)]
+                    [y        (* 1/2 unit)]
+                    [speed    (random 0.05 0.8)]
+                    [unit     unit]))))
+
+(define (draw)
+  (background 0)
+  (for ([blob blobs])
+    (blob.update)
+    (blob.draw)))
