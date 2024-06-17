6b3be4c603e2f11c626680dd3b1f4f0ab884410a
bernard.tatin@outlook.fr <BernardTatin>
2024-06-17 17:14:34 +0200

    optimisation

--- ca-bouge/vector-of-objects.rkt
+++ ca-bouge/vector-of-objects.rkt
@@ -1,5 +1,6 @@
 #lang sketching
 
+(require "../libs/timing.rkt")
 (require "../libs/draw-utils.rkt")
 (require "../libs/randomness.rkt")
 
@@ -8,7 +9,7 @@
 
 (define-syntax random-integer
     (syntax-rules ()
-        ((_ max-value) (number->int (random max-value)))))
+        ((_ max-value) (number->int (RND max-value)))))
 
 ;; from https://github.com/soegaard/sketching/blob/main/sketching-doc/sketching-doc/manual-examples/basics/vectors/vector-of-objects.rkt
 ;; but with more random
@@ -26,12 +27,12 @@
 
 (define get-r-factor
   (lambda(from to)
-    ; (RND from to)))
-    (let ((val (RND from to))
-          (val-sign (RND -1.0 1.0)))
-      (if (< val-sign 0)
-          (- val)
-          val))))
+    (RND from to)))
+    ; (let ((val (RND from to))
+    ;       (val-sign (RND -1.0 1.0)))
+    ;   (if (< val-sign 0)
+    ;       (- val)
+    ;       val))))
 
 ; Demonstrates how to create a vector of objects.
 (define sq-unit 15)
@@ -76,23 +77,34 @@
 (define unit  40)
 (define blobs (vector))
 
+(define get-color
+  (lambda(x cut-x y cut-y)
+    (cond
+      [(< x cut-x)
+       (if (< y cut-y)
+           "#ff3030"
+           "#8090ff")]
+      [(< y cut-y) "#1e90ff"]
+      [else "#1eff90"])))
+
 (define create-blobs
   (lambda(width height)
-    (:= blobs (for*/vector  ; for* is a nested loop
-                           ([xx (number->int (/ width  unit))]
-                            [yy (number->int (/ height unit))])
-                         (new Blob
-                              [x        (+ (* xx unit) (* 1/2 unit))]
-                              [y        (+ (* yy unit) (* 1/2 unit))]
-                              [bl-color (if (< xx (* 1/2 (/ width unit))) "#ff3030" "#1e90ff")]
-                              )))))
+    (let ((cut-x (* 1/2 width))
+          (cut-y (* 1/2 height)))
+      (:= blobs (for*/vector  ; for* is a nested loop
+                    ([xx (number->int (/ width  unit))]
+                     [yy (number->int (/ height unit))])
+                  (let ((nx (+ (* xx unit) (* 1/2 unit)))
+                        (ny (+ (* yy unit) (* 1/2 unit))))
+                    (new Blob
+                         [x        nx]
+                         [y        ny]
+                         [bl-color (get-color nx cut-x ny cut-y)]
+                         )))))))
 
 (define (setup)
   (size 640 800)
-  ;; 60 fps: 1.0 processeur, mouvement très fluides
-  ;; 30 fps: 0.6 processeur, mouvements légèrement saccadés
-  ;; =========== c'est quand même mieux que l'Amstrad CPC 64
-  (frame-rate 90)
+  (frame-rate 40)
   (no-stroke)
   ; (color-mode 'hsb)
   (create-blobs width height))
@@ -100,13 +112,30 @@
 (define (on-resize width height)
     (create-blobs width height))
 
+(define now #f)
+(define frames 0)
+
 (define (draw)
+  (cond
+    [(not now)
+     (:= now (current-inexact-milliseconds))
+     (:= frames 0)]
+    [else
+     (let ((dt (- (current-inexact-milliseconds) now)))
+       (++ frames)
+       (when (> dt 10000.0)
+         (display-all "fps: " (/ (* 1000 frames)  dt) "\n")
+         (:= now (current-inexact-milliseconds))
+         (:= frames 0)))])
+
   (background "#404040")
   (stroke-weight 4)
   (stroke "#aaaa44")
   (line (* 1/2 width) 0 (* 1/2 width) height)
+  (line 0 (* 1/2 height) width (* 1/2 height))
   (no-stroke)
-  (for ([blob blobs])
-    (blob.update))
+  (for ([i (in-range 10)])
+    (for ([blob blobs])
+      (blob.update)))
   (for ([blob blobs])
     (blob.draw)))
