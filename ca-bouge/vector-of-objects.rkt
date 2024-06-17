e5767933056a41156971f67e8dbb4efcadbfdd48
bernard.tatin@outlook.fr <BernardTatin>
2024-06-17 09:05:56 +0200

    No more bugs

--- ca-bouge/vector-of-objects.rkt
+++ ca-bouge/vector-of-objects.rkt
@@ -11,7 +11,7 @@
         ((_ max-value) (number->int (random max-value)))))
 
 ;; from https://github.com/soegaard/sketching/blob/main/sketching-doc/sketching-doc/manual-examples/basics/vectors/vector-of-objects.rkt
-;; but with more RND
+;; but with more random
 
 ; https://github.com/processing/processing-docs/tree/master/content/examples/Basics/Arrays/ArrayObjects
 ; Vector of objects.
@@ -26,56 +26,71 @@
 
 (define get-r-factor
   (lambda(from to)
-    (RND from to)))
-
-    ; (let ((val (RND from to))
-    ;       (val-sign (RND -1.0 1.0)))
-    ;   (if (< val-sign 0)
-    ;       (- val)
-    ;       val))))
+    ; (RND from to)))
+    (let ((val (RND from to))
+          (val-sign (RND -1.0 1.0)))
+      (if (< val-sign 0)
+          (- val)
+          val))))
 
 ; Demonstrates how to create a vector of objects.
 (define sq-unit 15)
 
 (class Blob Object
   ; "Constructor"
-  (init-field [x-offset 0]
-              [y-offset 0]
-              [x        0]
+  (init-field [x        0]
               [y        0]
               [x-dir    1]
               [y-dir    1]
-              [speed    (RND 0.5 1.0)]
+              [speed    (RND 0.8 1.2)]
               [bl-color (get-rand-color)])
   (super-new)    ; super class initialization
 
+  (define/private (move p p-dir pmax)
+    (let* ((n-speed (* (get-r-factor 0.8 1.2) speed p-dir))
+           (np (+ p n-speed)))
+      (cond
+        [(<= np sq-unit)
+          (values 1 (max sq-unit (+ p (abs n-speed))))]
+        [(>= np  (- pmax sq-unit))
+          (values -1 (max sq-unit (- p (abs n-speed))))]
+        [else
+          (values p-dir np)])))
+
 ; Methods
   (define/public (update)
-    (let* ((n-speed (* (get-r-factor 0.7 1.0) speed x-dir))
-           (nx (+ x n-speed)))
-      (cond
-        [(<= nx sq-unit)    ;; buggy test
-          (:= x-dir  1)
-          (:= x (max sq-unit (+ x (abs n-speed))))]
-        [(>= (+ x-offset nx) (- width sq-unit))
-          (:= x-dir -1)
-          (:= x (min (- width sq-unit) (- x (abs n-speed))))]
-        [else (:= x nx)]))
-    (let* ((n-speed (* (get-r-factor 0.7 1.0) speed y-dir))
-           (ny (+ y n-speed)))
-      (cond
-        [(<= ny sq-unit)    ;; buggy test
-          (:= y-dir  1)
-          (:= y (max sq-unit (+ y (abs n-speed))))]
-        [(>= (+ y-offset ny) (- height sq-unit))
-          (:= y-dir -1)
-          (:= y (min (- height sq-unit) (- y (abs n-speed))))]
-        [else (:= y ny)])))
+    (let-values (((n-dir nx) (move x x-dir width)))
+      (:= x-dir n-dir)
+      (:= x nx))
+    (let-values (((n-dir ny) (move y y-dir height)))
+      (:= y-dir n-dir)
+      (:= y ny))
+    ; (let* ((n-speed (* (get-r-factor 0.8 1.2) speed x-dir))
+    ;        (nx (+ x n-speed)))
+    ;   (cond
+    ;     [(<= nx sq-unit)
+    ;       (:= x-dir  1)
+    ;       (:= x (max sq-unit (+ x (abs n-speed))))]
+    ;     [(>= nx  (- width sq-unit))
+    ;       (:= x-dir -1)
+    ;       (:= x (min (- width sq-unit) (- x (abs n-speed))))]
+    ;     [else (:= x nx)]))
+    ; (let* ((n-speed (* (get-r-factor 0.8 1.2) speed y-dir))
+    ;        (ny (+ y n-speed)))
+    ;   (cond
+    ;     [(<= ny sq-unit)
+    ;       (:= y-dir  1)
+    ;       (:= y (max sq-unit (+ y (abs n-speed))))]
+    ;     [(>= ny (- height sq-unit))
+    ;       (:= y-dir -1)
+    ;       (:= y (min (- height sq-unit) (- y (abs n-speed))))]
+    ;     [else (:= y ny)]))
+        )
 
 
   (define/public (draw)
     (fill bl-color)
-    (ellipse (+ x x-offset) (+ y y-offset) 9 9)))
+    (ellipse x y 9 9)))
 
 ;;; ---------------
 
@@ -88,10 +103,8 @@
                            ([xx (number->int (/ width  unit))]
                             [yy (number->int (/ height unit))])
                          (new Blob
-                              [x-offset (* xx   unit)]
-                              [y-offset (* yy   unit)]
-                              [x        (* 1/2 unit)]
-                              [y        (* 1/2 unit)]
+                              [x        (+ (* xx unit) (* 1/2 unit))]
+                              [y        (+ (* yy unit) (* 1/2 unit))]
                               [bl-color (if (< xx (* 1/2 (/ width unit))) "#ff3030" "#1e90ff")]
                               )))))
 
